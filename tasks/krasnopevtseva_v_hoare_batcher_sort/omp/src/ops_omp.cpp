f88d2a5f635213d86c6254a61ccbd0e91c599c7a
NikaKrasnopevtseva <Krasnopevtseva_veronika@mail.ru>
2026-03-21 14:21:32 +0000

    completed

--- tasks/krasnopevtseva_v_hoare_batcher_sort/omp/src/ops_omp.cpp
+++ tasks/krasnopevtseva_v_hoare_batcher_sort/omp/src/ops_omp.cpp
@@ -139,7 +139,6 @@ void KrasnopevtsevaVHoareBatcherSortOMP::BatcherMerge(int thread_input_size, std
 #pragma omp parallel for default(none) shared(pointers, sizes, pack, step, thread_input_size, \
                                                   par_if_greater) if ((thread_input_size / step) > par_if_greater)
     for (int off = 0; off < pack / 2; ++off) {
-      // Используем size_t напрямую без cast, компилятор сам преобразует
       auto idx1 = static_cast<std::size_t>(2 * step) * static_cast<std::size_t>(off);
       auto idx2 = (static_cast<std::size_t>(2 * step) * static_cast<std::size_t>(off)) + static_cast<std::size_t>(step);
       BatcherMergeBlocksStep(pointers[idx1], sizes[idx1], pointers[idx2], sizes[idx2]);
