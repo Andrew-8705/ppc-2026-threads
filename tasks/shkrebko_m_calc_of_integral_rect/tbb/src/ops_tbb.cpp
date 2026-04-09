e692d61c0ff59dea1ef0a6e19954581806ef1b57
Shkrebko-Misha <michael.shkrebko@gmail.com>
2026-04-09 03:49:30 +0300

    fix

--- tasks/shkrebko_m_calc_of_integral_rect/tbb/src/ops_tbb.cpp
+++ tasks/shkrebko_m_calc_of_integral_rect/tbb/src/ops_tbb.cpp
@@ -94,27 +94,27 @@ double ShkrebkoMCalcOfIntegralRectTBB::ComputeBlockSum(std::size_t start_idx, st
       temp /= static_cast<std::size_t>(n_steps[idx]);
     }
   }
-    double block_sum = 0.0;
-    std::vector<double> point(dim);
+  double block_sum = 0.0;
+  std::vector<double> point(dim);
 
-    for (std::size_t iter = 0; iter < (end_idx - start_idx); ++iter) {
-      for (std::size_t dim_idx = 0; dim_idx < dim; ++dim_idx) {
-        point[dim_idx] = limits[dim_idx].first + ((static_cast<double>(indices[dim_idx]) + 0.5) * h[dim_idx]);
-      }
-      block_sum += local_input_.func(point);
-
-      int level = static_cast<int>(dim) - 1;
-      while (level >= 0) {
-        indices[level]++;
-        if (indices[level] < n_steps[level]) {
-          break;
-        }
-        indices[level] = 0;
-        level--;
-      }
+  for (std::size_t iter = 0; iter < (end_idx - start_idx); ++iter) {
+    for (std::size_t dim_idx = 0; dim_idx < dim; ++dim_idx) {
+      point[dim_idx] = limits[dim_idx].first + ((static_cast<double>(indices[dim_idx]) + 0.5) * h[dim_idx]);
     }
+    block_sum += local_input_.func(point);
 
-    return block_sum;
+    int level = static_cast<int>(dim) - 1;
+    while (level >= 0) {
+      indices[level]++;
+      if (indices[level] < n_steps[level]) {
+        break;
+      }
+      indices[level] = 0;
+      level--;
+    }
   }
+
+  return block_sum;
+}
 
 }  // namespace shkrebko_m_calc_of_integral_rect
