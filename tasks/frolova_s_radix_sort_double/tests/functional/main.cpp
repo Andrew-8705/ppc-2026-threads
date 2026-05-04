f690576e4f50c5a2ec7e4278ad40f1b437c3db8d
cuttty <frolova.sofya.05@mail.ru>
2026-05-04 12:51:16 +0300

    fix include

--- tasks/frolova_s_radix_sort_double/tests/functional/main.cpp
+++ tasks/frolova_s_radix_sort_double/tests/functional/main.cpp
@@ -11,7 +11,7 @@
 #include "frolova_s_radix_sort_double/common/include/common.hpp"
 #include "frolova_s_radix_sort_double/omp/include/ops_omp.hpp"
 #include "frolova_s_radix_sort_double/seq/include/ops_seq.hpp"
-#include "frolova_s_radix_sort_double/seq/include/ops_tbb.hpp"
+#include "frolova_s_radix_sort_double/tbb/include/ops_tbb.hpp"
 #include "util/include/func_test_util.hpp"
 #include "util/include/util.hpp"
 
