43e2d53bce1cd3b91b556928649db641b1048c06
ann lazareva <nyutik05@gmail.com>
2026-05-06 19:07:11 +0300

    fix9

--- tasks/lazareva_a_matrix_mult_strassen/all/include/ops_all.hpp
+++ tasks/lazareva_a_matrix_mult_strassen/all/include/ops_all.hpp
@@ -14,6 +14,14 @@ class LazarevaATestTaskALL : public BaseTask {
   }
   explicit LazarevaATestTaskALL(const InType &in);
 
+  static std::vector<double> Add(const std::vector<double> &a, const std::vector<double> &b, int n);
+  static std::vector<double> Sub(const std::vector<double> &a, const std::vector<double> &b, int n);
+  static void Split(const std::vector<double> &parent, int n, std::vector<double> &a11, std::vector<double> &a12,
+                    std::vector<double> &a21, std::vector<double> &a22);
+  static std::vector<double> Merge(const std::vector<double> &c11, const std::vector<double> &c12,
+                                   const std::vector<double> &c21, const std::vector<double> &c22, int n);
+  static std::vector<double> NaiveMult(const std::vector<double> &a, const std::vector<double> &b, int n);
+
  private:
   bool ValidationImpl() override;
   bool PreProcessingImpl() override;
@@ -29,13 +37,6 @@ class LazarevaATestTaskALL : public BaseTask {
   static int NextPowerOfTwo(int n);
   static std::vector<double> PadMatrix(const std::vector<double> &m, int old_n, int new_n);
   static std::vector<double> UnpadMatrix(const std::vector<double> &m, int old_n, int new_n);
-  static std::vector<double> Add(const std::vector<double> &a, const std::vector<double> &b, int n);
-  static std::vector<double> Sub(const std::vector<double> &a, const std::vector<double> &b, int n);
-  static void Split(const std::vector<double> &parent, int n, std::vector<double> &a11, std::vector<double> &a12,
-                    std::vector<double> &a21, std::vector<double> &a22);
-  static std::vector<double> Merge(const std::vector<double> &c11, const std::vector<double> &c12,
-                                   const std::vector<double> &c21, const std::vector<double> &c22, int n);
-  static std::vector<double> NaiveMult(const std::vector<double> &a, const std::vector<double> &b, int n);
   static std::vector<double> NaiveMultSequential(const std::vector<double> &a, const std::vector<double> &b, int n);
   static std::vector<double> NaiveMultParallel(const std::vector<double> &a, const std::vector<double> &b, int n);
   static std::vector<double> StrassenTBB(const std::vector<double> &a, const std::vector<double> &b, int n);
